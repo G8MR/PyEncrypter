7eb188c951b4e3f1d416a16bc84e4c742f29eb4b
AwesomeYoungCoder <87982540+AwesomeYoungCoder@users.noreply.github.com>
2021-11-17 13:06:49 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='PyEncrypter',
+version='0.0.1',
+description='A simple yet safe Python encryption library',
+url='https://github.com/AwesomeYoungCoder/PyEncrypter/',
+author='AwesomeYoungCoder',
+license='MIT',
+packages=['PyEncrypter'],
+zip_safe=False)
