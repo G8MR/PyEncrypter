58814dc3827f743398ec3f04b9d76c210a568e95
AwesomeYoungCoder <87982540+AwesomeYoungCoder@users.noreply.github.com>
2021-11-17 16:07:14 +0530

    Updated info for package

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
-from setuptools import setup
-setup(name='PyEncrypter',
-version='0.0.1',
-description='A simple yet safe Python encryption library',
-url='https://github.com/AwesomeYoungCoder/PyEncrypter/',
-author='AwesomeYoungCoder',
-license='MIT',
-packages=['PyEncrypter'],
-zip_safe=False)
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyEncrypter",                     
+    version="0.0.1",                        
+    author="AwesomeYoungCoder",
+    url = "https://github.com/AwesomeYoungCoder/PyEncrypter/",
+    description="A simple yet secure encryption package.",
+    long_description=long_description,      
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      
+    python_requires='>=3.6',                
+    py_modules=["PyEncrypter"],             
+    install_requires=[]                     
+)
