e25c8a0893a8283dbedcb063e116e7312ca41b5a
AwesomeYoungCoder <87982540+AwesomeYoungCoder@users.noreply.github.com>
2021-12-01 12:40:50 +0530

    Fixed error in which given text is misencrypted in some cases.

--- PyEncrypter/script.py
+++ PyEncrypter/script.py
@@ -1,3 +1,5 @@
+import logging
+
 class ObjInit:
     def __init__(self):
         self.layer1 = """ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_-!@#'"$%^&*()/?.,<>{[}]\|+=`~¦"""
@@ -6,10 +8,13 @@ class ObjInit:
         en1_length = int(len(data))
         en1_data = ""
         for i in range(0,en1_length):
-            x = int(self.layer1.index(data[i]))
-            if x < 10:
-                x = str(0)+str(x)
-            en1_data = en1_data + str(x)
+            try:
+                x = int(self.layer1.index(data[i]))
+                if x < 10:
+                    x = str(0)+str(x)
+                en1_data = en1_data + str(x)
+            except CharError:
+                logging.error("Error encountered : PyEncrypter does not support the given character. Please input a string of valid characters only.")
         return en1_data    
 
     def decrypt(self, data):
