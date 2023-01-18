5dcd4c8e39f1b0235b6ce58e433a6d132d7aa0e5
G8MR <87982540+G8MR@users.noreply.github.com>
2023-01-18 21:39:44 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyEncrypter",                     
-    version="0.2.2",                        
+    version="0.2.3",                        
     author="G8MR",
     url = "https://github.com/G8MR/PyEncrypter/",
     description="A simple yet secure encryption package.",
